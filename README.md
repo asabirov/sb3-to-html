ebe0487a9c55a59d1a13314c8ab15490dbab54a6
Artur Sabirov <artur.sabirov@apliteni.com>
2021-11-07 10:14:27 +0100

    more options

--- README.md
+++ README.md
@@ -6,10 +6,6 @@
       npm i -g ts-node
       bin/sb3-to-html examples/example_1.sb3 output/
 
-## Make a symlink
-
-      sudo ln -s bin/sb3-to-html /usr/local/bin/sb3-to-html
-
 ## Updating turbowarp
 
       bash /bin/download_turbowarp.sh 0.2.0
--- src/convert.ts
+++ src/convert.ts
@@ -25,6 +25,7 @@ import * as puppeteer from "puppeteer";
   await radio.click();
 
   // upload sb3
+  console.log("Uploading", projectFile);
   const file = await page.$("[type=file]");
   await file.uploadFile(projectFile);
 
@@ -33,14 +34,34 @@ import * as puppeteer from "puppeteer";
   await button.click();
 
   // wait
+  console.log("Waiting for next step");
   await page.waitForSelector("button.danger");
 
+  // enable controls
+
+  const options = [
+    "Show green",
+    "Show stop",
+    "Show fullscreen button",
+    "Support USB",
+  ];
+
+  await Promise.all(
+    options.map(async (label) => {
+      console.log("Clicking ", label);
+      const [element] = await page.$x(`//*[contains(., '${label}')]`);
+      await element.click();
+    })
+  );
+
   // submit
+  console.log("Submitting");
   const [submit] = await page.$x("//button[contains(., 'Package')]");
   await submit.click();
 
   // debugger
   // await page.screenshot({ path: "example.png" });
+  console.log("Waiting for file");
   await page.waitForTimeout(1000);
 
   await browser.close();
